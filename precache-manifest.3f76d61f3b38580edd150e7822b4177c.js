7600e269afc28cd3fa4140dae44121e9c8c5d406
Chris Del <drumgod101@gmail.com>
2019-01-12 23:24:46 -0500

    Updates

--- precache-manifest.3f76d61f3b38580edd150e7822b4177c.js
+++ precache-manifest.3f76d61f3b38580edd150e7822b4177c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/hackernews/static/js/runtime~main.da160233.js"
   },
   {
-    "revision": "218ece2beeb03e50d13f",
-    "url": "/hackernews/static/js/main.218ece2b.chunk.js"
+    "revision": "79eea927773f3663377c",
+    "url": "/hackernews/static/js/main.79eea927.chunk.js"
   },
   {
     "revision": "31edb0ddf58bcacc3b80",
     "url": "/hackernews/static/js/1.31edb0dd.chunk.js"
   },
   {
-    "revision": "218ece2beeb03e50d13f",
+    "revision": "79eea927773f3663377c",
     "url": "/hackernews/static/css/main.c10677fd.chunk.css"
   },
   {
-    "revision": "552f94c69f71415b06bfe7a6d73877e1",
+    "revision": "9d018549f47b39caea21d2e102598803",
     "url": "/hackernews/index.html"
   }
 ];
